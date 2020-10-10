d6bd9aebc9a946700cd824faa5c32b78cf3d1063
sunsiyan <sun_si_yan@163.com>
2020-10-10 15:59:58 +0800

    动画

--- src/router/index.js
+++ src/router/index.js
@@ -44,12 +44,12 @@ const routes = [
         meta: {title: '渲染函数API变化'},
         component: () => import('../views/renderTest')
     },
-    // {
-    //     path: '/functional',
-    //     name: 'functional',
-    //     meta: {title: '函数式组件'},
-    //     component: () => import('../views/Functional')
-    // },
+    {
+        path: '/transitionTest',
+        name: 'transitionTest',
+        meta: {title: '动画'},
+        component: () => import('../views/transitionTest')
+    },
 ];
 
 const router = createRouter({
---
+++ src/views/transitionTest.vue
@@ -0,0 +1,49 @@
+<template>
+    <div id="demoList">
+        <pre>
+            <p class="text">transition 类名变更</p>
+            <p class="text">v-enter ===>  v-enter-from</p>
+            <p class="text">v-leave ===>  v-leave-from</p>
+        </pre>
+
+        <button @click="show = !show">Toggle</button>
+
+        <transition name="fade">
+            <p class="name" v-if="show">hello</p>
+        </transition>
+    </div>
+</template>
+
+<script>
+    import {ref} from 'vue';
+
+    export default {
+        name: "transitionTest",
+        setup() {
+            const show = ref(true);
+
+            return {show}
+        }
+    }
+</script>
+
+<style scoped>
+    .fade-enter-active,
+    .fade-leave-active {
+        transition: opacity 0.5s ease;
+    }
+
+    .fade-enter-from,
+    .fade-leave-to {
+        opacity: 0;
+    }
+
+    p.text {
+        font-size: 20px;
+    }
+
+    p.name {
+        font-size: 20px;
+        margin-top: 20px;
+    }
+</style>
